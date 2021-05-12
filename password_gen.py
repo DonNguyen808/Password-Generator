333ca293a8a36529a2152a8ebb6f4f8b0fa4e89d
DonNguyen808 <DonNguyen808@gmail.com>
2021-05-12 12:47:41 -0400

    add check requirements

--- password_gen.py
+++ password_gen.py
@@ -1,11 +1,6 @@
 import string
 import random
 
-# intro
-print("Welcome to the password generator!")
-
-# ask the length of password
-length = int(input("how many characters for this password?"))
 
 # define data
 lower = string.ascii_lowercase
@@ -13,11 +8,47 @@ upper = string.ascii_uppercase
 num = string.digits
 symbols = string.punctuation
 
+# intro
+print("Welcome to the password generator!")
+
+# ask the length of password
+while True:
+    length = input("How many characters for this password?")
+    if length.isalpha():
+        print("Please enter a number")
+    if length.isdigit():
+        break
+
+add_all = ""
+
+l_confirm = input("Include lowercase?")
+if l_confirm == "yes":
+    add_all += lower
+else:
+    pass
+
+u_confirm = input("include uppercase?")
+if u_confirm == "yes":
+    add_all += upper
+else:
+    pass
+
+num_confirm = input("include numbers?")
+if num_confirm == "yes":
+    add_all += num
+else:
+    pass
+
+temp = random.sample(add_all, int(length))
+
+
+
+
 # combine all
 all = lower + upper + num + symbols
 
 #use random
-temp = random.sample(all, length)
+# temp = random.sample(all, length)
 
 # create the password
 password = "".join(temp)
