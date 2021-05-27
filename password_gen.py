60605382785b407e99292894c217208c64ead031
DonNguyen808 <DonNguyen808@gmail.com>
2021-05-27 12:27:52 -0400

    Added tkinter gui

--- password_gen.py
+++ password_gen.py
@@ -1,69 +1,94 @@
-import string
 import random
+from tkinter import *
+from tkinter.ttk import *
 
 
-# Define data
-lower = string.ascii_lowercase
-upper = string.ascii_uppercase
-num = string.digits
-symbols = string.punctuation
+# Function for calculation of password
+def low():
+    entry.delete(0, END)
 
-# Intro
-print("Welcome to the password generator!")
+    # Length of password
+    length = var1.get()
 
-# Ask the length of password
-while True:
-    length = input("How many characters for this password?")
-    if length.isalpha():
-        print("Please enter a number")
-    if length.isdigit():
-        break
+    # Define data
+    lower = "abcdefghijklmnopqrstuvwxyz"
+    upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    num = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789 !@#$%^&*()"
+    password = ""
 
-add_all = ""
 
-while True:
-    l_confirm = input("Include lowercase?")
-    if l_confirm == "yes":
-        add_all += lower
+    # Strength of low password
+    if var.get() == 1:
+        for i in range(0, length):
+            password = password + random.choice(lower)
+        return password
+
+    # Strength of medium password
+    elif var.get() == 0:
+        for i in range(0, length):
+            password = password + random.choice(upper)
+        return password
+
+    # Strength of strong password
+    elif var.get() == 3:
+        for i in range(0, length):
+            password = password + random.choice(num)
+        return password
+
     else:
-        pass
-
-    u_confirm = input("Include uppercase?")
-    if u_confirm == "yes":
-        add_all += upper
-    else:
-        pass
-
-    num_confirm = input("Include numbers?")
-    if num_confirm == "yes":
-        add_all += num
-    else:
-        pass
-
-    s_confirm = input("Include symbols?")
-    if s_confirm == "yes":
-        add_all += symbols
-    else:
-        pass
-
-    if add_all !="":
-        break
-
-temp = random.sample(add_all, int(length))
-
-# Combine all
-all = lower + upper + num + symbols
-
-# Create the password
-password = "".join(temp)
-
-print(password)
+        print("Please choose one")
 
 
 
+# Function for generate button
+def generate():
+    password1 = low()
+    entry.insert(10, password1)
+
+# Make the GUI window
+root = Tk()
+var = IntVar()
+var1 = IntVar()
+
+# Title
+root.title("Random Password Generator")
+
+
+# Password Label
+random_password = Label(root, text="Password")
+random_password.grid(row=0)
+
+# Entry for the password
+entry = Entry(root)
+entry.grid(row=0, column=1)
+
+length_label = Label(root, text="Length")
+length_label.grid(row=1)
+
+# Generate Button
+generate_button = Button(root, text="Generate", command=generate)
+generate_button.grid(row=0, column=2)
 
 
 
+# Radio button
+radio_low = Radiobutton(root, text="Low", variable=var, value=1)
+radio_low.grid(row=1, column=2, sticky="E")
+radio_medium = Radiobutton(root, text="Medium", variable=var, value=0)
+radio_medium.grid(row=1, column=3, sticky="E")
+
+radio_strong = Radiobutton(root, text="Strong", variable=var, value=3)
+radio_strong.grid(row=1, column=4, sticky="E")
+
+combo = Combobox(root, textvariable=var1)
+
+# Combo box for length of password
+combo['values'] = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15)
+combo.current(0)
+combo.bind("<<ComboboxSelected>>")
+combo.grid(column=1, row=1)
+
+root.mainloop()
 
 
 
